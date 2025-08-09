2d86a8e1e8ff1de2ba5eea964c0ace8bbf74dc60
高 <gao>
2025-08-09 13:55:08 +0800

    导航网页初版

---
+++ assets/js/tempfile_1754716973849.js
@@ -0,0 +1,27 @@
+const websiteData ={
+  "categories": [
+    {
+      "name": "一级分类",
+      "sites": [
+        {"name": "网站1", "url": "http://example.com"}
+      ]
+    },
+    {
+      "name": "主分类（含子分类）",
+      "subcategories": [
+        {
+          "name": "子分类1",
+          "sites": [
+            {"name": "网站2", "url": "http://example2.com"}
+          ]
+        },
+        {
+          "name": "子分类2",
+          "sites": [
+            {"name": "网站3", "url": "http://example3.com"}
+          ]
+        }
+      ]
+    }
+  ]
+}
---
+++ assets/js/websites.js
@@ -0,0 +1,273 @@
+// websites.js
+const websiteData = {
+    "categories": [
+        {
+            "name": "常用推荐",
+            "sites": [
+                {
+                    "name": "Dribbble",
+                    "url": "https://dribbble.com/",
+                    "description": "全球UI设计师作品分享平台。",
+                    "logo": "assets/images/logos/dribbble.png"
+                },
+                {
+                    "name": "猎云网",
+                    "url": "http://www.lieyunwang.com/",
+                    "description": "互联网创业项目推荐和创业创新资讯",
+                    "logo": "assets/images/logos/lieyunwang.png"
+                },
+                {
+                    "name": "nice design",
+                    "url": "http://www.niceui.cn/",
+                    "description": "nicedesign奈思设计是领先的用户体验设计与互联网品牌建设公司",
+                    "logo": "assets/images/logos/niceui.png"
+                },
+                {
+                    "name": "咕噜素材",
+                    "url": "https://www.yrucd.com/",
+                    "description": "质量很高的设计素材网站（良心推荐）",
+                    "logo": "assets/images/logos/gulusucai.png"
+                },
+                {
+                    "name": "Freebiesbug",
+                    "url": "https://freebiesbug.com/",
+                    "description": "高质量设计资源网站",
+                    "logo": "assets/images/logos/freebiesbug.png"
+                },
+                {
+                    "name": "西游-科学上网",
+                    "url": "https://xiyou4you.us",
+                    "description": "优秀的科学上网（略贵，但是贼稳，点击注册领取优惠券）",
+                    "logo": "assets/images/logos/xiyou.png"
+                }
+            ]
+        },
+        {
+            "name": "社区资讯",
+            "sites": [
+                {
+                    "name": "腾讯CDC",
+                    "url": "http://cdc.tencent.com/",
+                    "description": "腾讯CDC关注于互联网视觉设计、交互设计、用户研究、前端开发。",
+                    "logo": "assets/images/logos/cdc.png"
+                },
+                {
+                    "name": "TGideas",
+                    "url": "http://tgideas.qq.com/",
+                    "description": "TGideas隶属于腾讯公司互动娱乐业务系统的专业推广类设计团队",
+                    "logo": "assets/images/logos/tgideas.png"
+                },
+                {
+                    "name": "数英网",
+                    "url": "https://www.digitaling.com/",
+                    "description": "数字媒体及职业招聘网站",
+                    "logo": "assets/images/logos/digitaling.png"
+                },
+                {
+                    "name": "鸟哥笔记",
+                    "url": "http://www.niaogebiji.com/",
+                    "description": "移动互联网第一干货平台",
+                    "logo": "assets/images/logos/niaogebiji.png"
+                },
+                {
+                    "name": "古田路9号",
+                    "url": "http://www.gtn9.com/",
+                    "description": "国内专业品牌创意平台",
+                    "logo": "assets/images/logos/gtn9.png"
+                },
+                {
+                    "name": "云瑞",
+                    "url": "https://www.yrucd.com/",
+                    "description": "优秀设计资源的分享网站",
+                    "logo": "assets/images/logos/yrucd.png"
+                },
+                {
+                    "name": "雷锋网",
+                    "url": "https://www.leiphone.com/",
+                    "description": "人工智能和智能硬件领域的互联网科技媒体",
+                    "logo": "assets/images/logos/leiphone.png"
+                },
+                {
+                    "name": "36kr",
+                    "url": "http://36kr.com/",
+                    "description": "创业资讯、科技新闻",
+                    "logo": "assets/images/logos/36kr.png"
+                },
+                {
+                    "name": "互联网早读课",
+                    "url": "https://www.zaodula.com/",
+                    "description": "互联网行业深度阅读与学习平台",
+                    "logo": "assets/images/logos/zaodula.png"
+                },
+                {
+                    "name": "爱运营",
+                    "url": "http://www.iyunying.org/",
+                    "description": "网站运营人员学习交流，专注于网站产品运营管理、淘宝运营。",
+                    "logo": "assets/images/logos/iyunying.png"
+                },
+                {
+                    "name": "ISUX",
+                    "url": "https://isux.tencent.com/",
+                    "description": "腾讯社交用户体验设计部门",
+                    "logo": "assets/images/logos/isux.png"
+                },
+                {
+                    "name": "产品壹佰",
+                    "url": "http://www.chanpin100.com/",
+                    "description": "为产品经理爱好者提供最优质的产品资讯、原创内容和相关视频课程",
+                    "logo": "assets/images/logos/chanpin100.png"
+                }
+            ]
+        },
+        {
+            "name": "视频教程",
+            "sites": [
+                {
+                    "name": "没位道",
+                    "url": "http://so.ui001.com/",
+                    "description": "WEB UI免费视频公开课",
+                    "logo": "assets/images/logos/sousucai.png"
+                },
+                {
+                    "name": "慕课网",
+                    "url": "https://www.imooc.com/",
+                    "description": "程序员的梦工厂（有UI课程）",
+                    "logo": "assets/images/logos/imooc.png"
+                }
+            ]
+        },
+        {
+            "name": "设计素材",
+            "sites": [
+                {
+                    "name": "懒人图库",
+                    "url": "http://www.lanrentuku.com/",
+                    "description": "懒人图库专注于提供网页素材下载",
+                    "logo": "assets/images/logos/lanrentuku.png"
+                },
+                {
+                    "name": "easyicon",
+                    "url": "https://www.easyicon.net/",
+                    "description": "PNG、ICO、ICNS格式图标搜索、图标下载服务",
+                    "logo": "assets/images/logos/easyicon.png"
+                },
+                {
+                    "name": "flaticon",
+                    "url": "https://www.flaticon.com/",
+                    "description": "免费矢量图标库",
+                    "logo": "assets/images/logos/flaticon.png"
+                },
+                {
+                    "name": "咕噜素材",
+                    "url": "https://www.yrucd.com/",
+                    "description": "质量很高的设计素材网站（良心推荐）",
+                    "logo": "assets/images/logos/gulusucai.png"
+                },
+                {
+                    "name": "wallhalla",
+                    "url": "https://wallhalla.com/",
+                    "description": "高质量壁纸网站",
+                    "logo": "assets/images/logos/wallhalla.png"
+                }
+            ]
+        },
+        {
+            "name": "行业媒体",
+            "sites": [
+                {
+                    "name": "U一点",
+                    "url": "http://www.aliued.cn/",
+                    "description": "阿里巴巴（中国站）用户体验设计部博客U一点设计 UED团队",
+                    "logo": "assets/images/logos/aliuedcn.png"
+                },
+                {
+                    "name": "网易UEDC",
+                    "url": "http://uedc.163.com/",
+                    "description": "网易用户体验设计中心",
+                    "logo": "assets/images/logos/uedc.png"
+                },
+                {
+                    "name": "少数派",
+                    "url": "https://sspai.com/",
+                    "description": "高品质数字消费指南",
+                    "logo": "assets/images/logos/sspai.png"
+                },
+                {
+                    "name": "利器",
+                    "url": "http://liqi.io/",
+                    "description": "创造者和他们的工具",
+                    "logo": "assets/images/logos/liqi.png"
+                }
+            ]
+        },
+        {
+            "name": "交流社区",
+            "sites": [
+                {
+                    "name": "人人都是产品经理",
+                    "url": "http://www.woshipm.com/",
+                    "description": "产品经理、产品爱好者学习交流平台",
+                    "logo": "assets/images/logos/woshipm.png"
+                },
+                {
+                    "name": "UI中国",
+                    "url": "http://www.uigreat.com/",
+                    "description": "图形交互与界面设计交流、作品展示、学习平台。",
+                    "logo": "assets/images/logos/uigreat.png"
+                },
+                {
+                    "name": "站酷",
+                    "url": "http://www.zcool.com.cn/",
+                    "description": "设计师互动平台",
+                    "logo": "assets/images/logos/zcool.png"
+                }
+            ]
+        },
+        {
+            "name": "发现产品",
+            "sites": [
+                {
+                    "name": "Product Hunt",
+                    "url": "https://www.producthunt.com/",
+                    "description": "发现最新的产品",
+                    "logo": "assets/images/logos/producthunt.png"
+                },
+                {
+                    "name": "产品壹佰",
+                    "url": "http://www.chanpin100.com/",
+                    "description": "为产品经理爱好者提供最优质的产品资讯、原创内容和相关视频课程",
+                    "logo": "assets/images/logos/chanpin100.png"
+                },
+                {
+                    "name": "PMCAFF",
+                    "url": "http://www.pmcaff.com/",
+                    "description": "中国第一产品经理人气组织，专注于研究互联网产品",
+                    "logo": "assets/images/logos/pmcaff.png"
+                },
+                {
+                    "name": "Muse CC",
+                    "url": "https://creative.adobe.com/zh-cn/products/download/muse",
+                    "description": "无需利用编码即可进行网站设计。",
+                    "logo": "assets/images/logos/MuseCC.png"
+                },
+                {
+                    "name": "百度企业产品用户体验中心",
+                    "url": "http://ued.baidu.com/",
+                    "description": "百度企业产品用户体验中心",
+                    "logo": "assets/images/logos/euxbaidu.png"
+                },
+                {
+                    "name": "携程设计委员会",
+                    "url": "http://ued.ctrip.com/",
+                    "description": "携程设计委员会",
+                    "logo": "assets/images/logos/ctrip.png"
+                }
+            ]
+        }
+    ]
+};
+
+// 导出数据（如果使用模块系统）
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = websiteData;
+}
